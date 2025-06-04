73ff46c512207f607d232937071d8986a7058813
Oke2022 <xpdigita@gmail.com>
2025-06-04 18:54:43 -0400

    fix a bug

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,8 +1,19 @@
 io.on('connection', socket => {
   console.log('A user connected');
 
+  // Save username per socket
+  socket.on('set username', username => {
+    socket.username = username;
+  });
+
   socket.on('chat message', msg => {
-    io.emit('chat message', { msg, id: socket.id }); // send both message and sender's id
+    const messageData = {
+      msg,
+      username: socket.username || 'Anonymous',
+      id: socket.id,
+      timestamp: new Date().toLocaleTimeString()
+    };
+    io.emit('chat message', messageData);
   });
 
   socket.on('disconnect', () => {
