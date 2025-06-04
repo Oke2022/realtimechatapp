601afbf3de8f60f7ac77b87cb409d305f6a08400
Oke2022 <xpdigita@gmail.com>
2025-06-04 18:39:46 -0400

    fix a bug

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,24 +1,11 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
 io.on('connection', socket => {
   console.log('A user connected');
+
   socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+    io.emit('chat message', { msg, id: socket.id }); // send both message and sender's id
   });
+
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
-});
-
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
 });
